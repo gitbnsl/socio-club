e2f1ead8f2b172ddf4867ba6d4c6f96c5cd1479d
gitbnsl <90590676+gitbnsl@users.noreply.github.com>
2022-05-15 18:42:56 +0530

    changes in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,17 @@ const Footer = () => {
       <div className="container mt-5 p-5">
         <div className="row m-0 p-0">
           <hr />
-          <div className="col-4 text-center">
-            <button className="btn btn-sm btn-outline-light text-center"><i className="bi bi-linkedin px-1"></i>Linkdin</button>
-          </div>
+
           <div className="col-4 text-center">
             <button className="btn btn-sm btn-outline-light text-center"><i className="bi bi-instagram px-1"></i>Instagram</button>
           </div>
           <div className="col-4 text-center">
+            <button className="btn btn-sm btn-outline-light text-center"><i className="bi bi-linkedin px-1"></i>Linkdin</button>
+          </div>
+          <div className="col-4 text-center">
             <button className="btn btn-sm btn-outline-light text-center"><i className="bi bi-facebook px-1"></i>Facebook</button>
           </div>
-         
+
         </div>
       </div>
     </>
